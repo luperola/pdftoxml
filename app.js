5014a335008be763c20faa47128a6d62451ccef1
luperola <luigi.perolari@gmail.com>
2021-10-30 10:58:43 +0200

    'yaml5'

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`listening server at ${port}`);
 });
